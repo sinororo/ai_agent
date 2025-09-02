75d2b774d388ce513089fbb599d4a6ab183c4948
sinoro <signourmelo@gmail.com>
2025-09-02 17:42:31 +0800

    added verbose option, code to be adjusted

--- main.py
+++ main.py
@@ -10,23 +10,30 @@ def main():
 
     client = genai.Client(api_key=api_key)
 
+
     # check if  a command line argument for the prompt exists
 
-    if len(sys.argv) > 1:
-        user_prompt = sys.argv[1] # take the first argument in the prompt
+    if len(sys.argv) >= 2:
+        user_prompt = sys.argv[1]
+
+        response = client.models.generate_content(
+        model='gemini-2.0-flash-001',
+        contents=[user_prompt])
+
+        print(response.text) # take the first argument in the prompt
+
+        if len(sys.argv) >= 3 and sys.argv[2] == "--verbose":
+            print(response.text)
+            print(f"User prompt: {sys.argv[1]}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        
+        else:
+            print(response.text)
     else:
         # provide a default prompt
         print("Error: No prompt provided. Please provide a prompt as a command-line argument.")
         sys.exit(1)
-
-    response = client.models.generate_content(
-    model='gemini-2.0-flash-001',
-    contents=[user_prompt]
-    )
-
-    print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
