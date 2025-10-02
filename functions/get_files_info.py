8ca3ac1fac8a0e7c4b24cee0491e965135ea81da
sinoro <signourmelo@gmail.com>
2025-10-02 09:10:30 +0800

    put os.path that might help

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,2 +1,11 @@
 def get_files_info(working_directory, directory="."):
     pass
+
+    # os.path.abspath(): Get an absolute path from a relative path
+    # os.path.join(): Join two paths together safely (handles slashes)
+    # .startswith(): Check if a string starts with a substring
+    # os.path.isdir(): Check if a path is a directory
+    # os.listdir(): List the contents of a directory
+    # os.path.getsize(): Get the size of a file
+    # os.path.isfile(): Check if a path is a file
+    # .join(): Join a list of strings together with a separator
