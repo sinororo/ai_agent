d447700a14ac3f85e09b81aa1ea2f043291974b3
sinoro <signourmelo@gmail.com>
2025-10-07 05:48:18 +0800

    finished get_file_content.py

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -2,20 +2,24 @@ import os
 from config import MAX_CHARS
 
 def get_file_content(working_directory, file_path):
+    try:
 
-    abs_working = os.path.abspath(working_directory)
-    abs_target = os.path.abspath(os.path.join(working_directory, file_path))
+        abs_working = os.path.abspath(working_directory)
+        abs_target = os.path.abspath(os.path.join(working_directory, file_path))
 
-    if os.path.commonpath([abs_working, abs_target]) != abs_working:
-        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
-    
-    if not os.path.isfile(abs_target):
-        return f'Error: File not found or is not a regular file: "{file_path}"'
-    
-    with open(abs_target,"r") as f:
-        content = f.read()
-    if len(content) > MAX_CHARS:
-        content = content[:MAX_CHARS] + f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+        if os.path.commonpath([abs_working, abs_target]) != abs_working:
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        
+        if not os.path.isfile(abs_target):
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+        
+        with open(abs_target,"r") as f:
+            content = f.read()
+        if len(content) > MAX_CHARS:
+            content = content[:MAX_CHARS] + f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
 
-    return content
+        return content
     
+    except Exception as e:
+        return f"Error: {e}"
+        
--- tests.py
+++ tests.py
@@ -1,13 +1,29 @@
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
+
 
 # result for current directory
-print(get_files_info("calculator","."))
+# print(get_files_info("calculator","."))
 
-# result for pkg directory
-print(get_files_info("calculator","pkg"))
+# # result for pkg directory
+# print(get_files_info("calculator","pkg"))
 
-# result for /bin directory
-print(get_files_info("calculator","bin"))
+# # result for /bin directory
+# print(get_files_info("calculator","bin"))
 
-# result for ../ directory
-print(get_files_info("calculator","../"))
+# # result for ../ directory
+# print(get_files_info("calculator","../"))
+
+# print(get_file_content("calculator","lorem.txt"))
+
+# result for current directory
+print(get_file_content("calculator","main.py"))
+
+# # result for pkg directory
+print(get_file_content("calculator","pkg/calculator.py"))
+
+# # result for /bin directory
+print(get_file_content("calculator","/bin/cat"))
+
+# # result for ../ directory
+print(get_file_content("calculator","pkg/does_not_exist.py"))
