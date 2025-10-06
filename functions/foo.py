12b62a6f3d2edb57f716087ed92ba94493cec359
sinoro <signourmelo@gmail.com>
2025-10-06 10:03:46 +0800

    finished requirements 1 to 3 for ch2 l3

--- functions/foo.py
+++ functions/foo.py
@@ -1,26 +1,6 @@
-# python
 import os
 
-# def inside(wd, sub):
-#     abs_working = os.path.abspath(wd)
-#     abs_target = os.path.abspath(os.path.join(wd,sub))
-#     return abs_working, abs_target, os.path.commonpath([abs_working, abs_target])
 
-# print(inside("calculator", "."))          # True (same dir)
-# print(inside("calculator", "pkg"))        # True (child)
-# print(inside("calculator", "../"))        # False (parent)
-# print(inside("calculator", "/bin"))       # False (absolute outside)
-
-# python
-# import os
-# wd = ".."
-# print(os.path.abspath(os.path.join(wd, ".")))   # True
-# print(os.path.abspath(os.path.join(wd, "pkg")))  # True
-# print(os.path.abspath(os.path.join(wd, "nope")))  # False
-
-# print(os.path.isdir(os.path.abspath(os.path.join(wd, "."))))     # True
-# print(os.path.isdir(os.path.abspath(os.path.join(wd, "pkg"))))   # True
-# print(os.path.isdir(os.path.abspath(os.path.join(wd, "nope"))))  # False
-
-wd = "."
-print(os.listdir(wd))
+functions = os.path.abspath("functions")
+calculator = os.path.abspath("calculator")
+print(os.path.commonpath([functions,calculator]))
