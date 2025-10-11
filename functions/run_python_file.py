4923762f1990d2f66b79a4ff7923da6d0542e510
sinoro <signourmelo@gmail.com>
2025-10-12 07:33:29 +0800

    added try block and some vars on run_python_fie

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -1,2 +1,9 @@
 def run_python_file(working_directory, file_path, args=[]):
-    pass
+    try:
+        
+        abs_working = os.path.abspath(working_directory)
+        # ano nga ulit ibig sabihin nung nirerepresent nung  abs_target
+        abs_target = os.path.abspath(os.path.join(working_directory, file_path))
+        
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
