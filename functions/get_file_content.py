e2cbbc5e6b4cd978c091285818e28666539a7158
sinoro <signourmelo@gmail.com>
2025-10-21 18:44:48 +0800

    added schema get file content

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -22,4 +22,17 @@ def get_file_content(working_directory, file_path):
     
     except Exception as e:
         return f"Error: {e}"
-        
+
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
