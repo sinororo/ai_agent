390803d99535c948e89ca9bf2eb649921f1295f1
sinoro <signourmelo@gmail.com>
2025-10-28 13:35:21 +0800

    finished get_file_content.py

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,37 +1,44 @@
+# python
 import os
+from google.genai import types
 from config import MAX_CHARS
 
 def get_file_content(working_directory, file_path):
     try:
-
         abs_working = os.path.abspath(working_directory)
         abs_target = os.path.abspath(os.path.join(working_directory, file_path))
 
+        # Ensure target stays within working dir
         if os.path.commonpath([abs_working, abs_target]) != abs_working:
             return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
-        
+
+        # Ensure it’s a regular file
         if not os.path.isfile(abs_target):
             return f'Error: File not found or is not a regular file: "{file_path}"'
-        
-        with open(abs_target,"r") as f:
+
+        # Read text file (utf-8 by default)
+        with open(abs_target, "r", encoding="utf-8") as f:
             content = f.read()
+
+        # Truncate if too long
         if len(content) > MAX_CHARS:
-            content = content[:MAX_CHARS] + f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+            content = content[:MAX_CHARS] + f'\n[... File "{file_path}" truncated at {MAX_CHARS} characters]'
 
         return content
-    
+
     except Exception as e:
         return f"Error: {e}"
 
+
 schema_get_file_content = types.FunctionDeclaration(
     name="get_file_content",
-    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    description="Lists contents of a file, constrained to the working directory.",
     parameters=types.Schema(
         type=types.Type.OBJECT,
         properties={
-            "directory": types.Schema(
+            "file_path": types.Schema(
                 type=types.Type.STRING,
-                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+                description="The filepath to get file content from, relative to the working directory."
             ),
         },
     ),
